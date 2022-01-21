85247ee327861e49e44fac33f084a4fcf4e7140a
s22shadowl <jjerry409@gmail.com>
2022-01-22 02:31:10 +0800

    fix: 修正 api Key 安全性問題

--- src/API/WebAPI.js
+++ src/API/WebAPI.js
@@ -1,11 +1,12 @@
 import axios from "axios"
+require("dotenv").config()
 
 const isValidWords = (words) => {
   const option = {
     url: `https://wordsapiv1.p.rapidapi.com/words/${words}/definitions`,
     headers: {
       "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-      "x-rapidapi-key": "3bf35edbd1mshe6bebb95861ab32p1fc3a0jsn1f84699b1c73",
+      "x-rapidapi-key": `${process.env.Rapid_api_key}`,
     },
   }
   axios
